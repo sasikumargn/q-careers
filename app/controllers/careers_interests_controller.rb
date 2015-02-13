70cb53ec9dd859ebb8fc2ae4a6f11073cb7fa4c6
SasiKumar G N <snarayanasamy@qwinixtech.com>
2015-02-13 18:50:39 +0530

    removed bin from gitignore

--- app/controllers/careers_interests_controller.rb
+++ app/controllers/careers_interests_controller.rb
@@ -6,6 +6,22 @@ class CareersInterestsController < ApplicationController
 
 	def new
 		@carrer_interest = CareersInterest.new
+		@carrer_interest.build_attachment
 	end
+
+	def create
+		@carrer_interest = CareersInterest.new(careers_interest_params)
+		if @carrer_interest.save
+
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def careers_interest_params
+    params.require(:careers_interest).permit(:first_name, :last_name, :phone, :city, :state, :email, :skills, :attachment_attributes => [ :file ] )
+  end
 
 end
